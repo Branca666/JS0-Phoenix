e2f8220819a225a8aa52da903969032f9ef8a962
Ezequiel L. Auge <113632140+Branca666@users.noreply.github.com>
2023-09-17 20:04:53 -0300

    Add files via upload

---
+++ js0-variables.js
@@ -0,0 +1,44 @@
+/* Crea variables siguiendo las reglas de nomenclatura adecuadas. 
+
+1 - Crea 10 variables de tipo string (cadena de texto) y asignale cualquier texto. 
+let variableString = "Hola, mundo!";
+*/
+
+let nombre = "Ezequiel"
+let apellido = "Auge"
+let email = "ezequielauge@hotmail.com"
+let discord = "@branca666"
+let github = "@branca666"
+let materia = "Javascript"
+let anio = "2023"
+let mes = "Septiembre"
+let meta = "Convertirme en desarrollador"
+let meta2 = "Estar generando ingresos en el rubro para 2024/2025"
+
+/* 2 - Crea 10 variables de tipo number (numero) y asígnale cualquier número.
+let variableNumber = 100; */
+
+let variableNumer1 = 1;
+let variableNumer2 = 2;
+let variableNumer3 = 3;
+let variableNumer4 = 4;
+let variableNumer5 = 5;
+let variableNumer6 = 6;
+let variableNumer7 = 7;
+let variableNumer8 = 8;
+let variableNumer9 = 9;
+let variableNumer10 = 10;
+
+/* 3 - Crea 10 variables de tipo boolean (true/false) y asígnale cualquier valor.
+let variableBoolean = true; */
+
+let variableBoolean1 = true;
+let variableBoolean2 = false;
+let variableBoolean3 = true;
+let variableBoolean4 = false;
+let variableBoolean5 = true;
+let variableBoolean6 = false;
+let variableBoolean7 = true;
+let variableBoolean8 = false;
+let variableBoolean9 = true;
+let variableBoolean10 = false;
